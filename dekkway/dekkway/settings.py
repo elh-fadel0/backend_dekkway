fd38acdfe267241eb50bc8d1580c24430d848cfa
El hadji <elhadjifallou070@gmail.com>
2025-05-21 01:42:01 +0000

    procil

--- dekkway/dekkway/settings.py
+++ dekkway/dekkway/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
